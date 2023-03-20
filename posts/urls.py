16d70ca3f3a94561177c783932c02f9e6668183e
Kalilov Nurtilek <kalilovnurtilek1@gmail.com>
2023-03-20 21:33:14 +0600

    #5

--- posts/urls.py
+++ posts/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views import hello, get_index, get_about, get_contacts, get_post, update_post, delete_post
+from .views import hello, IndexView, AboutView, get_contacts,PostDetailView, PostCreateView, PostDeleteView, PostUpdateView
 
 urlpatterns = [
     path("hello/", hello, name="hello-view"),
-    path("", get_index, name="index-page"),
+    path("", IndexView.as_view(), name="index-page"),
     path("contacts/", get_contacts, name="contacts-page"),
-    path("about/", get_about, name="about-page"),
-    path("getpost/", get_post, name = "get-post"),
-    path("update/", update_post, name="update-post"),
-    path('delete/', delete_post, name="delete-post"),
-]
+    path("about/", AboutView.as_view(), name="about-page"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path("post/create/", PostCreateView.as_view(), name="post-create"),
+    path("post/delete/<int:pk>/", PostDeleteView.as_view(), name='post-delete'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
+
+    ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views import generic
+from django.urls import reverse_lazy
 
 def hello(request):
     # mylist  = [1,2,3,4]
@@ -25,22 +27,62 @@ def hello(request):
 # "Content-Dispasition": "attachment"}
     return HttpResponse(body, headers=headers, status=300)
 
-def get_index(request):
-    posts = Post.objects.filter(status=True)
-    context = {
-        "title" : "Main page",
-        "posts":posts,
-    }
-    return render(request, "posts/index.html", context=context)
 
 def get_contacts(request):
     return render(request, "posts/contacts.html", context=None)
 
-def get_about(request):
-    return render(request, "posts/about.html", context=None)
+# def get_index(request):
+#     posts = Post.objects.filter(status=True)
+#     context = {
+#         "title" : "Main page",
+#         "posts":posts,
+#     }
+#     return render(request, "posts/index.html", context=context)
+class IndexView(generic.ListView):
+    queryset = Post.objects.filter(status=True)
+    context_object_name = "posts"
+    # model = Post
+    template_name = "posts/index.html"
 
-def get_post(request):
-    return render(request, "posts/post_detail.html", context=None)
+class PostDetailView(generic.DetailView):
+    model = Post
+    context_object_name = "post"
+    template_name = "posts/post_detail.html"
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    template_name = 'posts/post_create.html'
+    fields =['title','content']
+    success_url = reverse_lazy("index-page")
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    template_name = 'posts/post_update.html'
+    fields =['title','content']
+    success_url = reverse_lazy("index-page")
+    
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy("index-page")
+    
+
+class AboutView(generic.TemplateView):
+    template_name ="posts/about.html"
+    extra_context = {
+        "title":"Страница о нас ",
+    }
+
+# CRUD___create_retriev_update_delete
+
+
+# def get_about(request):
+#     context = {
+#         "title":"Страница о нас ",
+#     }
+#     return render(request, "posts/about.html", context=context)
+
+# def get_post(request):
+#     return render(request, "posts/post_detail.html", context=None)
 
 def update_post(request):
     return render(request, "posts/post_update.html", context=None)
